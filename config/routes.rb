39d39bf38fee7b6b5e02dcc933b0662f4a3dd6d4
Sara Gorecki <sgorecki@gmail.com>
2014-03-11 17:23:19 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RailsAssessment::Application.routes.draw do
+
+  resources :posts, :comments
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
