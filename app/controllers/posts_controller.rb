5ea0735f8d60bd3e95d1790985f83ea554cabdd0
Sara Gorecki <sgorecki@gmail.com>
2014-03-11 19:21:24 -0400

    create new and create controllers, and new view for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,18 @@ class PostsController < ActionController::Base
     @posts = Post.all
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create!(params.permit(:post))
+
+    if @post.save
+      redirect_to @post
+    else
+      render new
+    end
+  end
+
 end
