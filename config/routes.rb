f8ece88f580ad7a79ba722c76a50e5974ba529a8
Sara Gorecki <sgorecki@gmail.com>
2014-03-11 21:35:14 -0400

    change routes so comments are nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 RailsAssessment::Application.routes.draw do
 
-  resources :posts, :comments
+  resources :posts do
+    resources :comments
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
