d5f5a754b4f84e5f94afbe796e07d5e4ca98346a
Sara Gorecki <sgorecki@gmail.com>
2014-03-11 21:42:29 -0400

    begin creating controller and view for new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,20 @@
 class CommentsController < ApplicationController
 
+  def new
+    @comment = Comment.new
+    @post = Post.find(params[:post_id])
+  end
+
+  def create
+    @comment = Comment.create(comment_params)
+    @post = Post.find(params[:post_id])
+    @post.comments << @comment
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author, :body)
+  end
+
 end
